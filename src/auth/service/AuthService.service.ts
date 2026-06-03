f009709ff93d8d20ae6b45627bdb4aa73569aaa8
Breno <147736256+BrenoNunes96@users.noreply.github.com>
2026-06-03 20:34:19 -0300

    Update AuthService.service.ts

--- src/auth/service/AuthService.service.ts
+++ src/auth/service/AuthService.service.ts
@@ -28,7 +28,7 @@ const payload = {sub:buscarUsuario?.usuario}
 return{
 id:buscarUsuario?.id,
 usuario:buscarUsuario?.usuario,
-token:`Bearer ${this.jwt.sign(payload)}`
+token:`${this.jwt.sign(payload)}`
 
 
 }
